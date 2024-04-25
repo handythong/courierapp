ffe020b1553b5cf7e90e3928c2d2a13f5c370e08
handy.thong <handythong@outlook.com>
2024-04-25 18:03:45 +0800

    added in test file

---
+++ CourierAppBackend/src/test/java/com/fdmgroup/courierapp/SgBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.courierapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
