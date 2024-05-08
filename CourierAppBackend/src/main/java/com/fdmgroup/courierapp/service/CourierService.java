f91e6336a8863eb3fe10a07840d46b1d843d9c3d
handy.thong <handythong@outlook.com>
2024-05-08 14:34:38 +0800

    register courier

--- CourierAppBackend/src/main/java/com/fdmgroup/courierapp/service/CourierService.java
+++ CourierAppBackend/src/main/java/com/fdmgroup/courierapp/service/CourierService.java
@@ -1,15 +1,23 @@
 package com.fdmgroup.courierapp.service;
 
+import com.fdmgroup.courierapp.model.Sender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fdmgroup.courierapp.model.Courier;
 import com.fdmgroup.courierapp.repository.CourierRepo;
 
+import java.util.Date;
+
 @Service
 public class CourierService {
 	
 	@Autowired
 	CourierRepo courierRepo;
-	
+
+	public Courier registerCourier(Courier newCourier) {
+		newCourier.setLastUpdated(new Date());
+		Courier createdCourier = courierRepo.save(newCourier);
+		return createdCourier;
+	}
 }
