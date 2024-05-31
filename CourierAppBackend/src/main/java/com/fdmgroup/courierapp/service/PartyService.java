48002296c786a7898bca2660ad46ca79f204d688
handy.thong <handythong@outlook.com>
2024-05-31 15:48:23 +0800

    delete party not found exception

--- CourierAppBackend/src/main/java/com/fdmgroup/courierapp/service/PartyService.java
+++ CourierAppBackend/src/main/java/com/fdmgroup/courierapp/service/PartyService.java
@@ -1,12 +1,9 @@
 package com.fdmgroup.courierapp.service;
 
-import com.fdmgroup.courierapp.exception.PartyNotFoundException;
 import com.fdmgroup.courierapp.model.Party;
 import com.fdmgroup.courierapp.repository.PartyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PartyService {
@@ -17,14 +14,4 @@ public class PartyService {
 	public Party saveParty(Party newParty) {
 		return partyRepo.save(newParty);
 	}
-
-	public Party findById(Long partyId) throws PartyNotFoundException{
-		Optional<Party> optParty = partyRepo.findById(partyId);
-		if (optParty.isPresent()) {
-			return optParty.get();
-		} else {
-			throw new PartyNotFoundException("Party not found");
-		}
-	}
-
 }
