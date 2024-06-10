a38d006f1fcab586fb56f29f0c252f708847e191
handy.thong <handythong@outlook.com>
2024-06-10 16:54:19 +0800

    remove query builders import

--- CourierAppBackend/src/main/java/com/fdmgroup/courierapp/repository/TripRepo.java
+++ CourierAppBackend/src/main/java/com/fdmgroup/courierapp/repository/TripRepo.java
@@ -1,7 +1,6 @@
 package com.fdmgroup.courierapp.repository;
 
 import com.fdmgroup.courierapp.model.*;
-import com.fdmgroup.courierapp.util.QueryBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
